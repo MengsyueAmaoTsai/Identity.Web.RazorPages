c61e4d1ee2d9d2b26bc1234e1e6c6362aaa7e0bb
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-11 23:12:00 +0800

    Add ApplicationInfo.cs

--- Src/Pages/About.cshtml.cs
+++ Src/Pages/About.cshtml.cs
@@ -1,15 +1,53 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Reflection;
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RichillCapital.Identity.Web.Pages;
 
 [AllowAnonymous]
-public class AboutViewModel(
-    ILogger<AboutViewModel> _logger) :
+public class AboutViewModel() :
     PageModel
 {
-    public void OnGet()
-    {
-    }
 }
 
+
+internal static class ApplicationInfo
+{
+    private const string NotPackaged = "Not packaged";
+
+    internal static string GetDisplayName()
+    {
+        return NotPackaged;
+    }
+
+    internal static string GetAssemblyVersion()
+    {
+        var attribute = typeof(Program).Assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>() ??
+            throw new InvalidOperationException();
+
+        return attribute.InformationalVersion;
+    }
+
+    internal static string GetPackageVersion()
+    {
+        return NotPackaged;
+    }
+
+    internal static string GetPackageUri()
+    {
+        return NotPackaged;
+    }
+
+    internal static string GetInstallLocation() => Assembly.GetExecutingAssembly().Location;
+
+    internal static string GetDotNetRuntimeVersion()
+    {
+        var attribute = typeof(object).Assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>() ??
+            throw new InvalidOperationException();
+
+        return attribute.InformationalVersion;
+    }
+}
