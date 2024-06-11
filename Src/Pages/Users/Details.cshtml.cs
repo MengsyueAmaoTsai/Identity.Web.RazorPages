eeac33a98f658ab735f1c7a8ef6b337340c98854
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-11 23:42:10 +0800

    User pages

--- Src/Pages/Users/Details.cshtml.cs
+++ Src/Pages/Users/Details.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RichillCapital.Identity.Web.Pages.Users;
@@ -7,4 +8,18 @@ namespace RichillCapital.Identity.Web.Pages.Users;
 public sealed class UserDetailsViewModel() :
     PageModel
 {
+    [BindProperty(SupportsGet = true, Name = "userId")]
+    public required string Id { get; init; }
+
+    public required string Email { get; set; }
+
+    public required string Name { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(
+        CancellationToken cancellationToken = default)
+    {
+        Name = "Meng-Syue Tsai";
+        Email = "test";
+        return Page();
+    }
 }
--- Src/Pages/Users/Index.cshtml.cs
+++ Src/Pages/Users/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RichillCapital.Identity.Web.Pages.Users;
@@ -7,4 +8,25 @@ namespace RichillCapital.Identity.Web.Pages.Users;
 public sealed class UsersViewModel() :
     PageModel
 {
+    public required IEnumerable<UserModel> Users { get; set; } = [];
+
+    public async Task<IActionResult> OnGetAsync(CancellationToken cancellationToken = default)
+    {
+        Users = [
+            new UserModel
+            {
+                Id = "1",
+                Email = "mengsyue.tsai@outlook.com",
+                Name = "Meng-Syue Tsai",
+            }
+        ];
+        return Page();
+    }
+}
+
+public sealed record UserModel
+{
+    public required string Id { get; init; }
+    public required string Email { get; init; }
+    public required string Name { get; init; }
 }
