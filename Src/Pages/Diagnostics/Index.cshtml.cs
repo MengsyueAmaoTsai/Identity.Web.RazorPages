5ea199c2bde3df78846471898d4c0802adc8ec8d
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-15 05:01:03 +0800

    Implemented diagnostic page

--- Src/Pages/Diagnostics/Index.cshtml.cs
+++ Src/Pages/Diagnostics/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RichillCapital.Identity.Web.Pages.Diagnostics;
@@ -6,4 +8,19 @@ namespace RichillCapital.Identity.Web.Pages.Diagnostics;
 [Authorize]
 public class DiagnosticsViewModel : PageModel
 {
+    public required AuthenticationProperties Properties { get; set; }
+    
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var result = await HttpContext.AuthenticateAsync();
+
+        if (!result.Succeeded)
+        {
+            return RedirectToPage("/users/signin", new { ReturnUrl = "/" });
+        }
+
+        Properties = result.Properties;
+
+        return Page();
+    }
 }
