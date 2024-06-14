4335ea0afd28869508f2bde1b8178fcf1cbb3088
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-15 05:42:26 +0800

    authenticate db user

--- Src/Pages/Users/SignIn/Index.cshtml.cs
+++ Src/Pages/Users/SignIn/Index.cshtml.cs
@@ -5,32 +5,75 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using RichillCapital.Domain;
+using RichillCapital.Domain.Common.Repositories;
+
 namespace RichillCapital.Identity.Web.Pages.Users.SignIn;
 
 [AllowAnonymous]
-public sealed class SignInViewModel :
+public sealed class SignInViewModel(
+    IReadOnlyRepository<User> _userRepository) :
     PageModel
 {
     [BindProperty(SupportsGet = true)]
     public required string ReturnUrl { get; set; }
 
+    [BindProperty]
+    public required string Email { get; init; }
+
+    [BindProperty]
+    public required string Password { get; init; }
+
+    [BindProperty]
+    public bool RememberMe { get; init; }
+
     public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken = default)
     {
+        var emailResult = Domain.Email.From(Email);
+
+        if (emailResult.IsFailure)
+        {
+            ModelState.AddModelError(emailResult.Error.Code, emailResult.Error.Message);
+            return Page();
+        }
+
+        var email = emailResult.Value;
+
+        var maybeUser = await _userRepository.FirstOrDefaultAsync(
+            user => user.Email == email,
+            cancellationToken);
+
+        if (maybeUser.IsNull)
+        {
+            ModelState.AddModelError("UserNotFound", "User not found.");
+            return Page();
+        }
+
+        var user = maybeUser.Value;
+
+        if (!(user.Password == Password))
+        {
+            ModelState.AddModelError("InvalidPassword", "Invalid password.");
+            return Page();
+        }
+
         var principle = new ClaimsPrincipal(
             new ClaimsIdentity(
             [
-                new Claim("sub", "1"),
-                new Claim(ClaimTypes.Name, "John Doe"),
-                new Claim(ClaimTypes.Email, "mengsyue.tsai@outlook.com"),
+                new Claim("sub", user.Id.Value),
+                new Claim(ClaimTypes.Email, user.Email.Value),
+                new Claim(ClaimTypes.Name, user.Name),
                 new Claim(ClaimTypes.Role, "Admin"),
             ],
             RichillCapitalAuthenticationSchemes.Cookie));
 
-        var properties = new AuthenticationProperties
-        {
-            IsPersistent = false,
-            ExpiresUtc = DateTimeOffset.UtcNow.AddSeconds(30),
-        };
+        var properties = RememberMe ?
+            new AuthenticationProperties
+            {
+                IsPersistent = false,
+                ExpiresUtc = DateTimeOffset.UtcNow.AddSeconds(30),
+            } :
+            null;
 
         await HttpContext.SignInAsync(principle, properties);
 
