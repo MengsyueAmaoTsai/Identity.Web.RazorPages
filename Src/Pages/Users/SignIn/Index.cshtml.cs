54cd72f7733ea8f80fdeb8764919d77781b1a2f2
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-14 22:39:43 +0800

    add sign in and sign up page

---
+++ Src/Pages/Users/SignIn/Index.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RichillCapital.Identity.Web.Pages.Users.SignIn;
+
+public sealed class SignInViewModel : 
+    PageModel
+{
+}
---
+++ Src/Pages/Users/SignUp/Index.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RichillCapital.Identity.Web.Pages.Users.SignUp;
+
+public sealed class SignUpViewModel : 
+    PageModel
+{
+}
