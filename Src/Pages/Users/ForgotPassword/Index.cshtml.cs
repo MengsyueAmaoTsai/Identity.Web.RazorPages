c9b756abb50d6b2c9575f056df09ca51e6115932
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-15 07:50:49 +0800

    reset password

--- Src/Pages/Users/ForgotPassword/Index.cshtml.cs
+++ Src/Pages/Users/ForgotPassword/Index.cshtml.cs
@@ -1,7 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using RichillCapital.Domain;
+using RichillCapital.Domain.Common.Repositories;
 
 namespace RichillCapital.Identity.Web.Pages.Users.ForgotPassword;
 
-public sealed class ForgotPasswordViewModel : PageModel
+public sealed class ForgotPasswordViewModel(
+    IRepository<User> _userRepository,
+    IUnitOfWork _unitOfWork) : PageModel
 {
+    [BindProperty]
+    public required string Email { get; init; }
+
+    public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken)
+    {
+        var emailResult = Domain.Email.From(Email);
+
+        if (emailResult.IsFailure)
+        {
+            ModelState.AddModelError(emailResult.Error.Code, emailResult.Error.Message);
+
+            return Page();
+        }
+
+        var email = emailResult.Value;
+
+        var maybeUser = await _userRepository.FirstOrDefaultAsync(user => user.Email == email, cancellationToken);
+
+        if (maybeUser.IsNull)
+        {
+            ModelState.AddModelError("UserNotFound", "User with this email does not exist");
+            return Page();
+        }
+
+        var passwordResetToken = string.Empty;
+        var resetUrl = string.Empty;
+
+        return Page();
+    }
 }
---
+++ Src/Pages/Users/ResetPassword/Index.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RichillCapital.Identity.Web.Pages.Users.ResetPassword;
+
+[AllowAnonymous]
+public sealed class ResetPasswordViewModel : PageModel
+{
+}
