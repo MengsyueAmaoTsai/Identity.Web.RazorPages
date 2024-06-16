4c1cb206f972d6cb809170648e0459717568374c
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-17 04:56:51 +0800

    implemented sign out

--- Src/Pages/Identity/SignOut.cshtml.cs
+++ Src/Pages/Identity/SignOut.cshtml.cs
@@ -1,7 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using RichillCapital.SharedKernel.Monads;
 
 namespace RichillCapital.Identity.Web.Pages.Identity;
 
 public sealed class SignOutViewModel : PageModel
 {
+    public async Task<IActionResult> OnPostAsync(string returnUrl, CancellationToken cancellationToken = default)
+    {
+        var result = await SignOutAsync(cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return Page();
+        }
+
+        if (!string.IsNullOrEmpty(returnUrl))
+        {
+            return LocalRedirect(returnUrl);
+        }
+        else
+        {
+            // This needs to be a redirect so that the browser performs a new
+            // request and the identity for the user gets updated.
+            return RedirectToPage();
+        }
+    }
+
+    private async Task<Result> SignOutAsync(
+        CancellationToken cancellationToken = default)
+    {
+        await HttpContext.SignOutAsync(RichillCapitalAuthenticationSchemes.DefaultCookieScheme);
+        return Result.Success;
+    }
 }
