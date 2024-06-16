efe86dbc56598803a67947942ac643656011d800
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-17 04:52:11 +0800

    add sign out page

---
+++ Src/Pages/Identity/Manage/Index.cshtml.cs
@@ -0,0 +1,5 @@
+﻿namespace RichillCapital.Identity.Web.Pages.Identity;
+
+public sealed class IdentityManageViewModel
+{
+}
---
+++ Src/Pages/Identity/SignOut.cshtml.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RichillCapital.Identity.Web.Pages.Identity;
+
+public sealed class SignOutViewModel : PageModel
+{
+}
