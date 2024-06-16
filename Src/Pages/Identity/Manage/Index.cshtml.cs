ed721c41f425299a6a15e2977a0cf1029a95e51d
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-17 05:16:09 +0800

    manage index

--- Src/Pages/Identity/Manage/Index.cshtml.cs
+++ Src/Pages/Identity/Manage/Index.cshtml.cs
@@ -2,14 +2,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using RichillCapital.Domain;
+using RichillCapital.Domain.Common.Repositories;
+using RichillCapital.UseCases.Common;
+
 namespace RichillCapital.Identity.Web.Pages.Identity;
 
 [Authorize]
-public sealed class IdentityManageViewModel : PageModel
+public sealed class IdentityManageViewModel(
+    ICurrentUser _currentUser,
+    IReadOnlyRepository<User> _userRepository) :
+    PageModel
 {
-    public async Task<IActionResult> OnGetAsync(
-        CancellationToken cancellationToken = default)
+    public required string StatusMessage { get; init; }
+
+    public required string UserName { get; set; }
+
+    public required string PhoneNumber { get; set; }
+    public async Task<IActionResult> OnGetAsync(CancellationToken cancellationToken = default)
     {
+        var maybeUser = await _userRepository.GetByIdAsync(_currentUser.Id, cancellationToken);
+
+        if (maybeUser.IsNull)
+        {
+            return NotFound($"Unable to load user with ID '{_currentUser.Id}'.");
+        }
+
+        var user = maybeUser.Value;
+
+        UserName = user.Name.Value;
+        PhoneNumber = user.PhoneNumber.Value;
+
         return Page();
     }
 }
