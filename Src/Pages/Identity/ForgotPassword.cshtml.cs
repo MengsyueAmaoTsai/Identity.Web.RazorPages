b1820409f43505a5ff69f60919ae1fe3b3379c25
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-17 06:36:19 +0800

    add forgot password confrimation page

--- Src/Pages/Identity/ForgotPassword.cshtml.cs
+++ Src/Pages/Identity/ForgotPassword.cshtml.cs
@@ -1,11 +1,42 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using RichillCapital.Domain;
+using RichillCapital.Domain.Common.Repositories;
 
 namespace RichillCapital.Identity.Web.Pages.Identity;
 
 [AllowAnonymous]
-public sealed class ForgoetPasswordViewModel
+public sealed class ForgotPasswordViewModel(
+    IReadOnlyRepository<User> _userRepository) :
+    PageModel
 {
     [BindProperty]
     public required string Email { get; init; }
+
+    public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken = default)
+    {
+        var validationResult = Domain.Email.From(Email);
+
+        if (validationResult.IsFailure)
+        {
+            ModelState.AddModelError(validationResult.Error.Code, validationResult.Error.Message);
+            return Page();
+        }
+
+        var email = validationResult.Value;
+
+        var maybeUser = await _userRepository.FirstOrDefaultAsync(user => user.Email == email, cancellationToken);
+
+        if (maybeUser.IsNull)
+        {
+            ModelState.AddModelError("Email", "User with this email does not exist.");
+            return Page();
+        }
+
+        // generate and send password reset token
+
+        return Redirect("./forgotPasswordConfirmation");
+    }
 }
---
+++ Src/Pages/Identity/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RichillCapital.Identity.Web.Pages.Identity;
+
+[AllowAnonymous]
+public class ForgotPasswordConfirmationViewModel : PageModel
+{
+}
