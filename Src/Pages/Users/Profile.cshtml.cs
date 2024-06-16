ebed9d1e358892fdeb1ae1796fafa453ee748fb3
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-16 18:15:25 +0800

    profile page

--- Src/Pages/Users/Profile.cshtml.cs
+++ Src/Pages/Users/Profile.cshtml.cs
@@ -1,7 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using RichillCapital.Domain;
+using RichillCapital.Domain.Common.Repositories;
+using RichillCapital.Identity.Web.Models.Users;
+using RichillCapital.UseCases.Common;
 
 namespace RichillCapital.Identity.Web.Pages.Users;
 
-public sealed class UserProfileViewModel : PageModel
+public sealed class UserProfileViewModel(
+    ICurrentUser _currentUser,
+    IReadOnlyRepository<User> _userRepository) :
+    PageModel
 {
+    public required UserModel User { get; set; }
+    public async Task<IActionResult> OnGetAsync(CancellationToken cancellationToken = default)
+    {
+        var maybeUser = await _userRepository.GetByIdAsync(_currentUser.Id, cancellationToken);
+
+        if (maybeUser.IsNull)
+        {
+            return NotFound();
+        }
+
+        var user = maybeUser.Value;
+
+        User = new UserModel
+        {
+            Id = user.Id.Value,
+            Email = user.Email.Value,
+            Name = user.Name,
+            AvatarUrl = string.Empty,
+        };
+
+        return Page();
+    }
 }
