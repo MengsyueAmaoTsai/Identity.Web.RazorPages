9b0370c005607d6cb8dfc4f93deb5e8cb09b7651
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-16 13:31:50 +0800

    add page routes

---
+++ Src/Pages/PageRoutes.cs
@@ -0,0 +1,8 @@
+﻿namespace RichillCapital.Identity.Web.Pages;
+
+internal static class PageRoutes
+{
+    public const string Index = "/";
+    public const string Error = "/error";
+    public const string About = "/about";
+}
