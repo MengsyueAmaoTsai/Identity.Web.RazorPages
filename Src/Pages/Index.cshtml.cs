00751c34eabd154c637fbbd89b5f4c4abb15e308
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-17 04:15:36 +0800

    diagnostics

--- Src/Pages/Index.cshtml.cs
+++ Src/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RichillCapital.Identity.Web.Pages;
@@ -7,4 +9,19 @@ namespace RichillCapital.Identity.Web.Pages;
 public class IndexViewModel :
     PageModel
 {
+    public required AuthenticationProperties Properties { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(CancellationToken cancellationToken = default)
+    {
+        var authenticationResult = await HttpContext.AuthenticateAsync();
+
+        if (!authenticationResult.Succeeded)
+        {
+            return RedirectToPage("/error");
+        }
+
+        Properties = authenticationResult.Properties;
+
+        return Page();
+    }
 }
