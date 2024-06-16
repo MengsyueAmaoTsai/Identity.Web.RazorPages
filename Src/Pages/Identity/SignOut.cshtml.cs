ff23235e1ce2fb685b474f4784024a943502b0ee
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-17 05:08:50 +0800

    add signed out

--- Src/Pages/Identity/SignOut.cshtml.cs
+++ Src/Pages/Identity/SignOut.cshtml.cs
@@ -1,18 +1,51 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Services;
+
+using IdentityModel;
+
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 using RichillCapital.SharedKernel.Monads;
+using RichillCapital.UseCases.Common;
 
 namespace RichillCapital.Identity.Web.Pages.Identity;
 
 [Authorize]
-public sealed class SignOutViewModel : PageModel
+public sealed class SignOutViewModel(
+    ICurrentUser _currentUser,
+    IIdentityServerInteractionService _interactionService) : 
+    PageModel
 {
     public async Task<IActionResult> OnPostAsync(string returnUrl, CancellationToken cancellationToken = default)
     {
+        var logoutId = await _interactionService.CreateLogoutContextAsync();
+
+        if (!_currentUser.IsAuthenticated)
+        {
+            return RedirectToPage("/identity/singedOut", new { LogoutId = logoutId });
+        }
+            
         var result = await SignOutAsync(cancellationToken);
+
+        var provider = User.FindFirst(JwtClaimTypes.IdentityProvider)?.Value;
+
+        // if it's a local login we can ignore this workflow
+        if (provider is not null && provider != IdentityServerConstants.LocalIdentityProvider)
+        {
+            if (await HttpContext.GetSchemeSupportsSignOutAsync(provider))
+            {
+                // build a return URL so the upstream provider will redirect back
+                // to us after the user has logged out. this allows us to then
+                // complete our single sign-out processing.
+                string url = Url.Page("/identity/signedOut", new { LogOutId = logoutId }) ?? "/identity/signed-out";
+
+                // this triggers a redirect to the external provider for sign-out
+                return SignOut(new AuthenticationProperties { RedirectUri = url }, provider);
+            }
+        }
 
         if (result.IsFailure)
         {
@@ -34,7 +67,7 @@ public sealed class SignOutViewModel : PageModel
     private async Task<Result> SignOutAsync(
         CancellationToken cancellationToken = default)
     {
-        await HttpContext.SignOutAsync(RichillCapitalAuthenticationSchemes.DefaultCookieScheme);
+        await HttpContext.SignOutAsync();
         return Result.Success;
     }
 }
