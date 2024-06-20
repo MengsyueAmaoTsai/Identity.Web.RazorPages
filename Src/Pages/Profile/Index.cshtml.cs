fe71fe0a46140ba9e2f435a5ec8cd8cae2728d42
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-20 19:55:22 +0800

    profile page

--- Src/Pages/Profile/Index.cshtml.cs
+++ Src/Pages/Profile/Index.cshtml.cs
@@ -2,27 +2,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using RichillCapital.Domain.Common.Repositories;
+using RichillCapital.Domain.Users;
+using RichillCapital.UseCases.Common;
+
 namespace RichillCapital.Identity.Web.Pages.Profile;
 
 [Authorize]
-public sealed class ProfileViewModel() :
+public sealed class ProfileViewModel(
+    IUserManager _userManager,
+    ICurrentUser _currentUser) :
     PageModel
 {
     [TempData]
     public required string StatusMessage { get; init; }
 
-    public required bool HasPassword { get; set; }
-
-    public required string PhoneNumber { get; set; }
-
-    public required bool EnabledTwoFactorAuthentication { get; set; }
-
-    public required bool BrowserRemembered { get; set; }
-
-    public required string AuthenticatorKey { get; set; }
+    public required string Email { get; set; }
 
     public async Task<IActionResult> OnGetAsync(CancellationToken cancellationToken = default)
     {
+        var userResult = await _userManager.GetByIdAsync(_currentUser.Id, cancellationToken);
+
+        if (userResult.IsFailure)
+        {
+            return NotFound(userResult.Error.Message);
+        }
+
+        var user = userResult.Value;
+
+        Email = user.Email.Value;
         // var userResult = await _userService.GetByIdAsync(_currentUser.Id, cancellationToken);
 
         // if (userResult.IsFailure)
