9a4c953ab35063d859b255bce73bdf4234979431
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-20 11:44:26 +0800

    Remove page route

--- Src/Pages/PageRoutes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RichillCapital.Identity.Web.Pages;
-
-internal static class PageRoutes
-{
-    public const string Index = "/";
-    public const string Error = "/error";
-    public const string About = "/about";
-}
