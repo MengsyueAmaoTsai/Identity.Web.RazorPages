6a53e37727c51e87ef66ff8ac930becd56b97ccf
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-20 12:00:12 +0800

    redirect to profile page

--- Src/Pages/Identity/SignIn.cshtml.cs
+++ Src/Pages/Identity/SignIn.cshtml.cs
@@ -90,7 +90,7 @@ public sealed class SignInViewModel(
         {
             return Url.IsLocalUrl(ReturnUrl) ?
                 Redirect(ReturnUrl) : string.IsNullOrEmpty(ReturnUrl) ?
-                    Redirect("~/") : throw new Exception("invalid return URL");
+                    RedirectToPage("/identity/profile/index") : throw new Exception("invalid return URL");
         }
 
         return context.IsNativeClient() ?
