e3109a2db3c98a13e26bcd3999e8949f70fb331a
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-08 22:11:23 +0800

    Change password ui

--- Src/Pages/Password/Change/Index.cshtml.cs
+++ Src/Pages/Password/Change/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,12 +8,15 @@ namespace RichillCapital.Identity.Web.Pages.Password.Change;
 public sealed class PasswordChangeViewModel : PageModel
 {
     [BindProperty]
+    [Display(Name = "Current password")]
     public required string CurrentPassword { get; init; }
 
     [BindProperty]
+    [Display(Name = "New password")]
     public required string NewPassword { get; init; }
 
     [BindProperty]
+    [Display(Name = "Reenter password")]
     public required string ReenterPassword { get; init; }
 
     public async Task<IActionResult> OnPostAsync()
---
+++ Src/Pages/Password/Reset/Index.cshtml.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RichillCapital.Identity.Web.Pages.Password.Reset;
+
+public sealed class PasswordResetViewModel : PageModel
+{
+}
