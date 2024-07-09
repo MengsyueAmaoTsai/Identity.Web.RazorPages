8454865cba674c1032a2309721e5e806ce5ace19
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-09 23:19:23 +0800

    stay signed in page

--- Src/Pages/Identity/EnterPassword/Index.cshtml.cs
+++ Src/Pages/Identity/EnterPassword/Index.cshtml.cs
@@ -1,20 +1,9 @@
-using Duende.IdentityServer.Events;
-using Duende.IdentityServer.Services;
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-using RichillCapital.Domain.Common.Repositories;
-using RichillCapital.Domain.Users;
-using RichillCapital.SharedKernel.Monads;
-
 namespace RichillCapital.Identity.Web.Pages.Identity.EnterPassword;
 
-public class EnterPasswordViewModel(
-    ISignInManager _signInManager,
-    IReadOnlyRepository<User> _userRepository,
-    IIdentityServerInteractionService _interactionService,
-    IEventService _eventService) : 
+public class EnterPasswordViewModel() :
     PageModel
 {
     [BindProperty(SupportsGet = true)]
@@ -30,47 +19,14 @@ public class EnterPasswordViewModel(
 
     public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken = default)
     {
-        var email = Email
-            .From(EmailAddress)
-            .ThrowIfFailure()
-            .ValueOrDefault;
+        TempData["Password"] = Password;
 
-        var signInResult = await _signInManager.PasswordSignInAsync(
-            email,
-            Password,
-            isPersistent: true,
-            lockoutOnFailure: true);
-
-        if (signInResult.IsFailure)
-        {
-            return Page();
-        }
-
-        var maybeUser = await _userRepository
-            .FirstOrDefaultAsync(user => user.Email == email, cancellationToken)
-            .ThrowIfNull();
-
-        var user = maybeUser.Value;
-
-        var context = await _interactionService.GetAuthorizationContextAsync(ReturnUrl);
-
-        await _eventService.RaiseAsync(new UserLoginSuccessEvent(
-            user.Email.Value,
-            user.Id.Value,
-            user.Name.Value,
-            clientId: context is null || context.Client is null ?
-                string.Empty :
-                context.Client.ClientId));
-
-        if (context is null)
-        {
-            return Url.IsLocalUrl(ReturnUrl) ?
-                Redirect(ReturnUrl) : string.IsNullOrEmpty(ReturnUrl) ?
-                    RedirectToPage("/profile/index") : throw new Exception("invalid return URL");
-        }
-
-        return context.IsNativeClient() ?
-            this.LoadingPage(ReturnUrl) :
-            Redirect(ReturnUrl);
+        return RedirectToPage(
+            "/identity/staySignedIn/index",
+            new
+            {
+                ReturnUrl,
+                EmailAddress,
+            });
     }
 }
---
+++ Src/Pages/Identity/StaySignedIn/Index.cshtml.cs
@@ -0,0 +1,71 @@
+using Duende.IdentityServer.Events;
+using Duende.IdentityServer.Services;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using RichillCapital.Domain.Common.Repositories;
+using RichillCapital.Domain.Users;
+using RichillCapital.SharedKernel.Monads;
+
+namespace RichillCapital.Identity.Web.Pages.Identity.StaySignedIn;
+
+[AllowAnonymous]
+public sealed class StaySignInViewModel(
+    ISignInManager _signInManager,
+    IReadOnlyRepository<User> _userRepository,
+    IIdentityServerInteractionService _interactionService,
+    IEventService _eventService) : PageModel
+{
+    [BindProperty(SupportsGet = true)]
+    public required string ReturnUrl { get; init; }
+
+    [BindProperty(SupportsGet = true)]
+    public required string EmailAddress { get; init; }
+
+    [BindProperty]
+    public required bool StaySignedIn { get; init; }
+
+    public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken = default)
+    {
+        var email = Email
+            .From(EmailAddress)
+            .ThrowIfFailure()
+            .ValueOrDefault;
+
+        var password = TempData["Password"] as string ?? string.Empty;
+
+        var signInResult = await _signInManager.PasswordSignInAsync(
+             email,
+             password,
+             isPersistent: StaySignedIn,
+             lockoutOnFailure: true);
+
+        var maybeUser = await _userRepository
+            .FirstOrDefaultAsync(user => user.Email == email, cancellationToken)
+            .ThrowIfNull();
+
+        var user = maybeUser.Value;
+        var context = await _interactionService.GetAuthorizationContextAsync(ReturnUrl);
+
+        await _eventService.RaiseAsync(new UserLoginSuccessEvent(
+            user.Email.Value,
+            user.Id.Value,
+            user.Name.Value,
+            clientId: context is null || context.Client is null ?
+                string.Empty :
+                context.Client.ClientId));
+
+        if (context is null)
+        {
+            return Url.IsLocalUrl(ReturnUrl) ?
+                Redirect(ReturnUrl) : string.IsNullOrEmpty(ReturnUrl) ?
+                    RedirectToPage("/profile/index") : throw new Exception("invalid return URL");
+        }
+
+        return context.IsNativeClient() ?
+            this.LoadingPage(ReturnUrl) :
+            Redirect(ReturnUrl);
+    }
+}
