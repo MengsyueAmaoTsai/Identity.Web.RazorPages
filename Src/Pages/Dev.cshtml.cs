33e87d7274804f12302c6bfff14f2690164c4f1d
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-13 10:06:50 +0800

    add sign in page

--- Src/Pages/Dev.cshtml.cs
+++ Src/Pages/Dev.cshtml.cs
@@ -1,7 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RichillCapital.Identity.Web.Pages;
 
 public sealed class DevViewModel : PageModel
 {
+    private static class ErrorMessages
+    {
+        internal const string InvalidEmail = 
+            "Enter a valid email address, phone number, or Skype name.";
+
+        internal const string EmailNotFound = 
+            "We couldn't find an account with that username. Try another, or <a>get a new Microsoft account</a>.";
+    }
+        
+    public IActionResult OnPostBack()
+    {
+        Console.WriteLine("OnPostBack");
+        return Page();
+    }
+
+    public IActionResult OnPostNext()
+    {
+        Console.WriteLine("OnPostEmail");
+        return Page();
+    }
 }
