cd218310b3101c71cea6f809992677722fc3e1b8
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-14 22:51:19 +0800

    Add _ValidationScriptsPartial

--- Src/Pages/SignIn/Index.cshtml.cs
+++ Src/Pages/SignIn/Index.cshtml.cs
@@ -7,11 +7,16 @@ namespace RichillCapital.Identity.Web.Pages.SignIn;
 
 public sealed class SignInViewModel : PageModel
 {
+    private static class Errors
+    {
+        internal const string InvalidEmailAddress = "Enter a valid email address, phone number, or Skype name.";
+    }
+
     [BindProperty(SupportsGet = true)]
     public required string ReturnUrl { get; init; }
 
     [BindProperty]
-    [Required]
-    [EmailAddress]
+    [Required(ErrorMessage = Errors.InvalidEmailAddress)]
+    [EmailAddress(ErrorMessage = Errors.InvalidEmailAddress)]
     public required string EmailAddress { get; init; }
 }
