036832200d3fddf5ed5f8f0eb7eddecb81b5cb05
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-16 01:51:32 +0800

    Birth date page

--- Src/Pages/IdentityViewModel.cs
+++ Src/Pages/IdentityViewModel.cs
@@ -3,8 +3,15 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RichillCapital.Identity.Web.Pages;
 
-public abstract class IdentityViewModel : PageModel
+public abstract class IdentityViewModel : ViewModel
 {
     [BindProperty(SupportsGet = true)]
     public required string ReturnUrl { get; init; }
+}
+
+
+public abstract class ViewModel : PageModel
+{
+    protected IActionResult RedirectToErrorPage() => RedirectToPage("/error/index");
+    protected IActionResult RedirectToProfilePage() => RedirectToPage("/profile/index");
 }
