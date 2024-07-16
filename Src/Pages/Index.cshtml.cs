edefadec382b255de8274e6a2c7d0919d37b45df
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-17 07:08:42 +0800

    Change password button

--- Src/Pages/Index.cshtml.cs
+++ Src/Pages/Index.cshtml.cs
@@ -1,12 +1,32 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+using RichillCapital.Domain.Common.Repositories;
+using RichillCapital.Domain.Users;
+using RichillCapital.UseCases.Common;
 
 namespace RichillCapital.Identity.Web.Pages;
 
 [Authorize]
-public class IndexViewModel() :
-    PageModel
+public class IndexViewModel(
+    ICurrentUser _currentUser,
+    IReadOnlyRepository<User> _userRepository) :
+    ViewModel
 {
-    public required AuthenticationProperties Properties { get; set; }
+    public required User CurrentUser { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(CancellationToken cancellationToken = default)
+    {
+        var maybeUser = await _userRepository
+            .GetByIdAsync(_currentUser.Id, cancellationToken);
+
+        if (maybeUser.IsNull)
+        {
+            return ErrorPage();
+        }
+        
+        CurrentUser = maybeUser.Value;
+
+        return Page();
+    }
 }
