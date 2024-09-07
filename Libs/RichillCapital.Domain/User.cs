65ee748b964daed1d78ac8a850d6c97b81bc183c
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-08 03:18:07 +0800

    Add user entity

---
+++ Libs/RichillCapital.Domain/User.cs
@@ -0,0 +1,25 @@
+﻿using RichillCapital.SharedKernel;
+
+namespace RichillCapital.Domain;
+
+public sealed class User : Entity<UserId>
+{
+    public User(
+        UserId id,
+        string name,
+        Email email,
+        string passwordHash,
+        DateTimeOffset createdTimeUtc) 
+        : base(id)
+    {
+        Name = name;
+        Email = email;
+        PasswordHash = passwordHash;
+        CreateTimeUtc = createdTimeUtc;
+    }
+
+    public string Name { get; private set; }
+    public Email Email { get; private set; }
+    public string PasswordHash { get; private set; }
+    public DateTimeOffset CreateTimeUtc { get; private set; }
+}
