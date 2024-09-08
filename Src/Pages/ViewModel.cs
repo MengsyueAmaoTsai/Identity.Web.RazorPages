a3c867406cd2bc557fe6c0cff1d0ef73946cde36
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-09 03:05:07 +0800

    Remove sign up name

--- Src/Pages/ViewModel.cs
+++ Src/Pages/ViewModel.cs
@@ -33,14 +33,6 @@ public abstract class ViewModel : PageModel
             EmailAddress = email.Value,
         });
 
-    protected IActionResult SignUpName(string returnUrl, string emailAddress) => RedirectToPage(
-        "/signUp/name/index",
-        new
-        {
-            returnUrl,
-            emailAddress,
-        });
-
     protected IActionResult SignUpBirthdate(string returnUrl, string emailAddress, string name) => RedirectToPage(
         "/signUp/birthdate/index",
         new
